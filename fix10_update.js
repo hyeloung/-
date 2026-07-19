29a3aec3b1adcb79d492a4139224f166be67a13c
hyeloung <hyeloung@users.noreply.github.com>
2026-07-19 23:38:21 +0900

    Update screen 10 with the correct fasting instruction image

---
+++ fix10_update.js
@@ -0,0 +1,9 @@
+﻿const fs = require('fs');
+
+// Copy image
+fs.copyFileSync('C:\\Users\\LG\\.gemini\\antigravity\\brain\\9099f388-8581-4fa1-b388-668740ae9b49\\media__1784471557598.png', 'C:\\Users\\LG\\Desktop\\PO\\page10.png');
+
+// Update index.html
+let html = fs.readFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', 'utf8');
+html = html.replaceAll('v=35', 'v=36');
+fs.writeFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', html, 'utf8');
