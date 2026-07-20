fd7b6371eaadef8583dd278ccfdc147441f904b3
hyeloung <hyeloung@users.noreply.github.com>
2026-07-21 04:27:44 +0900

    Make images fill the screen height using 100vh

---
+++ fix_fullscreen.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+
+// Update index.html
+let html = fs.readFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', 'utf8');
+// replace height: auto; with height: 100vh;
+html = html.replace(/height: auto;/g, 'height: 100vh;');
+html = html.replaceAll('v=45', 'v=46');
+fs.writeFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', html, 'utf8');
