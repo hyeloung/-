308f4fa424410adc9946564fe5fdcccbf4a74143
hyeloung <hyeloung@users.noreply.github.com>
2026-07-21 05:13:43 +0900

    Reduce desktop zoom scale to 135vw

---
+++ fix_bump.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let html = fs.readFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', 'utf8');
+html = html.replace('style.css?v=40', 'style.css?v=41');
+html = html.replace('app.js?v=40', 'app.js?v=41');
+fs.writeFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', html, 'utf8');
