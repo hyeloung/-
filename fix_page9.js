085496784a6cbf422e9bdc8b903ad071bf94ce14
hyeloung <hyeloung@users.noreply.github.com>
2026-07-22 06:34:52 +0900

    Replace page 9 image and update cache version to 48

---
+++ fix_page9.js
@@ -0,0 +1,4 @@
+﻿const fs = require('fs');
+let html = fs.readFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', 'utf8');
+html = html.replace('page9.png?v=47', 'page9.png?v=48');
+fs.writeFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', html, 'utf8');
