c84f9fcc19d904853a2314fd0a73163c53d99578
hyeloung <hyeloung@users.noreply.github.com>
2026-07-22 06:49:44 +0900

    Replace page 5 image again with the newly provided image

---
+++ fix_page5_again.js
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+fs.copyFileSync('C:\\Users\\LG\\.gemini\\antigravity\\brain\\9099f388-8581-4fa1-b388-668740ae9b49\\media__1784670442902.png', 'C:\\Users\\LG\\Desktop\\PO\\page5.png');
+
+let html = fs.readFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', 'utf8');
+html = html.replace('page5.png?v=48', 'page5.png?v=49');
+fs.writeFileSync('C:\\Users\\LG\\Desktop\\PO\\index.html', html, 'utf8');
+
+console.log("Image copied and cache bumped.");
